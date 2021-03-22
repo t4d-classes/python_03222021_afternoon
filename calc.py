97152139c9df81a4db8cb072c599a4fff2ae668e
Eric Greene <eric@t4d.io>
2021-03-22 17:11:10 -0400

    add solution 2

---
+++ calc.py
@@ -0,0 +1,32 @@
+
+result = 0
+
+
+command = input("Please enter a command > ")
+
+
+while command:
+
+    if command == "add":
+        operand = float(input("Please enter an operand > "))
+        result = result + operand
+        print(f"Result: {result}")
+    elif command == "subtract":
+        operand = float(input("Please enter an operand > "))
+        result = result - operand
+        print(f"Result: {result}")
+    elif command == "multiply":
+        operand = float(input("Please enter an operand > "))
+        result = result * operand
+        print(f"Result: {result}")
+    elif command == "divide":
+        operand = float(input("Please enter an operand > "))
+        result = result / operand
+        print(f"Result: {result}")
+    elif command == "clear":
+        result = 0
+        print(f"Result: {result}")
+    else:
+        print("unknown command, please try again")
+
+    command = input("Please enter a command > ")
